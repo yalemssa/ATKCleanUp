3749b8c501b57785952056d267db2e3d96695705
Maureen Callahan <maureen.callahan@gmail.com>
2015-02-03 14:22:47 -0500

    Update ExtendAbbreviatedMonths.sql

--- ExtendAbbreviatedMonths.sql
+++ ExtendAbbreviatedMonths.sql
@@ -1,21 +1,21 @@
 /* January */
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Jan.','January')
 where dateExpression LIKE '%Jan.%'
 or dateExpression LIKE '%Jan.'
 or dateExpression LIKE 'Jan.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Jan','January')
 where dateExpression LIKE '%Jan'
 or dateExpression LIKE '%Jan %'
 or dateExpression LIKE 'Jan %';
 /*February*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Feb.','February')
 where dateExpression LIKE '%Feb.%'
 or dateExpression LIKE '%Feb.'
 or dateExpression LIKE 'Feb.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Feb','February')
 where dateExpression LIKE '%Feb'
 or dateExpression LIKE '%Feb %'
@@ -26,133 +26,133 @@ or dateExpression LIKE '%Febuary%'
 or dateExpression LIKE 'Febuary%'
 or dateExpression LIKE '%Febuary';
 /*March*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Mar.','March')
 where dateExpression LIKE '%Mar.%'
 or dateExpression LIKE '%Mar.'
 or dateExpression LIKE 'Mar.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Mar','March')
 where dateExpression LIKE '%Mar'
 or dateExpression LIKE '%Mar %'
 or dateExpression LIKE 'Mar %';
 /*April*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Apr.','April')
 where dateExpression LIKE '%Apr.%'
 or dateExpression LIKE '%Apr.'
 or dateExpression LIKE 'Apr.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Apr','April')
 where dateExpression LIKE '%Apr'
 or dateExpression LIKE '%Apr %'
 or dateExpression LIKE 'Apr %';
 /*June*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Jun.','June')
 where dateExpression LIKE '%Jun.%'
 or dateExpression LIKE '%Jun.'
 or dateExpression LIKE 'Jun.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Jun','June')
 where dateExpression LIKE '%Jun'
 or dateExpression LIKE '%Jun %'
 or dateExpression LIKE 'Jun %';
 /*August*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Aug.','August')
 where dateExpression LIKE '%Aug.%'
 or dateExpression LIKE '%Aug.'
 or dateExpression LIKE 'Aug.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Aug','August')
 where dateExpression LIKE '%Aug'
 or dateExpression LIKE '%Aug %'
 or dateExpression LIKE 'Aug %';
 /*September*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Sep.','September')
 where dateExpression LIKE '%Sep.%'
 or dateExpression LIKE '%Sep.'
 or dateExpression LIKE 'Sep.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Sep','September')
 where dateExpression LIKE '%Sep'
 or dateExpression LIKE '%Sep %'
 or dateExpression LIKE 'Sep %';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Sept.','September')
 where dateExpression LIKE '%Sept.%'
 or dateExpression LIKE '%Sept.'
 or dateExpression LIKE 'Sept.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Sept','September')
 where dateExpression LIKE '%Sept'
 or dateExpression LIKE '%Sept %'
 or dateExpression LIKE 'Sept %';
 /*October*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Oct.','October')
 where dateExpression LIKE '%Oct.%'
 or dateExpression LIKE '%Oct.'
 or dateExpression LIKE 'Oct.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Oct','October')
 where dateExpression LIKE '%Oct'
 or dateExpression LIKE '%Oct %'
 or dateExpression LIKE 'Oct %';
 /*November*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Nov.','November')
 where dateExpression LIKE '%Nov.%'
 or dateExpression LIKE '%Nov.'
 or dateExpression LIKE 'Nov.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Nov','November')
 where dateExpression LIKE '%Nov'
 or dateExpression LIKE '%Nov %'
 or dateExpression LIKE 'Nov %';
 /*December*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Dec.','December')
 where dateExpression LIKE '%Dec.%'
 or dateExpression LIKE '%Dec.'
 or dateExpression LIKE 'Dec.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'Dec','December')
 where dateExpression LIKE '%Dec'
 or dateExpression LIKE '%Dec %'
 or dateExpression LIKE 'Dec %';
 /*Other abbreviations*/
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'c.','circa')
 where dateExpression LIKE '% c.%'
 or dateExpression LIKE '% C.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'c.','Circa')
 where dateExpression LIKE 'c.%'
 or dateExpression LIKE 'C.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'ca.','circa')
 where dateExpression LIKE '% ca.%'
 or dateExpression LIKE '% Ca.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'ca.','Circa')
 where dateExpression LIKE 'ca.%'
 or dateExpression LIKE 'Ca.%';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'ca','circa')
 where dateExpression LIKE '% ca %'
 or dateExpression LIKE '% Ca %';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'ca','Circa')
 where dateExpression LIKE 'ca %'
 or dateExpression LIKE 'Ca %';
-update yale_dev.ResourcesComponents
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'n.d.','Undated')
 where dateExpression LIKE 'n.d.%'
-or dateExpression LIKE 'n.d.'
-update yale_dev.ResourcesComponents
+or dateExpression LIKE 'n.d.';
+update ResourcesComponents
 set dateExpression = replace(dateExpression, 'n.d.','undated')
 where dateExpression LIKE '%n.d.'
 or dateExpression LIKE '%n.d.%';
