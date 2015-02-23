702f1fab73f16cf03713f186972176bd576d6830
Maureen Callahan <maureen.callahan@gmail.com>
2015-02-23 16:51:21 -0500

    Update CleanUpSeriesIds.sql

--- CleanUpSeriesIds.sql
+++ CleanUpSeriesIds.sql
@@ -1,15 +1,24 @@
+/* This looks for the string "series" in the component unique identifier and makes sure that the level is series. */
 Update ResourcesComponents
 Set resourceLevel = 'series'
-where subdivisionIdentifier like '%Series%';
+where subdivisionIdentifier like 'Series%';
+/* This looks for the string "subseries" in the component unique identifier and makes sure that the level is subseries. */
+Update ResourcesComponents
+Set resourceLevel = 'subseries'
+where subdivisionIdentifier like 'Subseries%';
+/* This looks for the string "Accession" in the component unique identifier and makes sure that the level is otherlevel and otherlevel is set to accession. */
 Update ResourcesComponents
 Set resourceLevel = 'otherLevel'
 and otherLevel = 'Accession'
 where subdivisionIdentifier like '%Accession%';
+/* This strips the string "Accession" from the component unique identifier */
 Update ResourcesComponents
 Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Accession', '');
+/* This strips the string "Series" (or series all caps) from the component unique identifier */
 Update ResourcesComponents
 Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Series', '');
 Update ResourcesComponents
 Set subdivisionIdentifier = replace(subdivisionIdentifier, 'SERIES', '');
+/* This trims any leading or trailing whitespace from the component unique identifier */
 Update ResourcesComponents
 Set subdivisionIdentifier = trim(subdivisionIdentifier);
