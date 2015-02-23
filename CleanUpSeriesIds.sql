fdca17be7690a792f864fa94706635a2e956e954
Maureen Callahan <maureen.callahan@gmail.com>
2015-02-23 16:08:24 -0500

    Create CleanUpSeriesIds.sql
    We currently have the string "series" in our component unique identifiers. These need to be stripped and moved to a level attribute.

---
+++ CleanUpSeriesIds.sql
@@ -0,0 +1,13 @@
+Update ResourcesComponents
+Set resourceLevel = 'series'
+where subdivisionIdentifier like '%Series%';
+Update ResourcesComponents
+Set resourceLevel = 'otherLevel'
+and otherLevel = 'Accession'
+where subdivisionIdentifier like '%Accession%';
+Update ResourcesComponents
+Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Accession', '');
+Update ResourcesComponents
+Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Series', '');
+Update ResourcesComponents
+Set subdivisionIdentifier = trim(subdivisionIdentifier);
