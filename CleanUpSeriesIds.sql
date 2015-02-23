2258f1a685c786f09a9aefd4ae34ba19ad58f48c
Maureen Callahan <maureen.callahan@gmail.com>
2015-02-23 16:15:33 -0500

    Update CleanUpSeriesIds.sql

--- CleanUpSeriesIds.sql
+++ CleanUpSeriesIds.sql
@@ -10,4 +10,6 @@ Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Accession', '');
 Update ResourcesComponents
 Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Series', '');
 Update ResourcesComponents
+Set subdivisionIdentifier = replace(subdivisionIdentifier, 'SERIES', '');
+Update ResourcesComponents
 Set subdivisionIdentifier = trim(subdivisionIdentifier);
