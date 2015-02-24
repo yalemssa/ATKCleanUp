285a386d2d380d0dcacf7fe76bbbc40726869e4b
Maureen Callahan <maureen.callahan@gmail.com>
2015-02-24 15:01:15 -0500

    Removed changes to accession strings

--- CleanUpSeriesIds.sql
+++ CleanUpSeriesIds.sql
@@ -6,14 +6,6 @@ where subdivisionIdentifier like 'Series%';
 Update ResourcesComponents
 Set resourceLevel = 'subseries'
 where subdivisionIdentifier like 'Subseries%';
-/* This looks for the string "Accession" in the component unique identifier and makes sure that the level is otherlevel and otherlevel is set to accession. */
-Update ResourcesComponents
-Set resourceLevel = 'otherlevel'
-and otherLevel = 'Accession'
-where subdivisionIdentifier like '%Accession%';
-/* This strips the string "Accession" from the component unique identifier */
-Update ResourcesComponents
-Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Accession', '');
 /* This strips the string "Series" (or series all caps) from the component unique identifier */
 Update ResourcesComponents
 Set subdivisionIdentifier = replace(subdivisionIdentifier, 'Series', '');
